d4f1a3dd227b47a0472b62437ae1bc4531170e23
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-27 08:59:06 +0100

    Created RabbitMQ connection provider

---
+++ src/AbstractedRabbitMQ/Setup/ConnectionProvider.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractedRabbitMQ.Setup
+{
+    public class ConnectionProvider : IConnectionProvider
+    {
+        private ConnectionFactory _factory;
+        private IConnection _connection;
+        private bool _disposed;
+        public ConnectionProvider(string url, string? clientProvidedName)
+        {
+            _factory = new ConnectionFactory
+            {
+                Uri = new Uri(url),
+            };
+            if (clientProvidedName != null)
+            {
+                _factory.ClientProvidedName = clientProvidedName;
+            }
+            _connection = _factory.CreateConnection();
+        }
+        public IConnection GetConnection() => _connection;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            if (disposing)
+                _connection.Dispose();
+            _disposed = true;
+        }
+    }
+}
---
+++ src/AbstractedRabbitMQ/Setup/IConnectionProvider.cs
@@ -0,0 +1,10 @@
+﻿using RabbitMQ.Client;
+
+namespace AbstractedRabbitMQ.Setup
+{
+    public interface IConnectionProvider
+    {
+        void Dispose();
+        IConnection GetConnection();
+    }
+}
