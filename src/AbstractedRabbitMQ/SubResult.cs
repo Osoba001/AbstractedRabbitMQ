1def9de513d5ff3357e9b51e368cfccd75e9bfbf
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-27 20:02:04 +0100

    Generic type of action result

---
+++ src/AbstractedRabbitMQ/SubResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractedRabbitMQ
+{
+    public class SubResult<T>
+    {
+        public SubResult()
+        {
+            IsSuccess = true;
+        }
+        public bool IsSuccess { get;private set; }
+        public string? ReasonPhrase { get; private set; }
+        public void AddError(string errorMessage)
+        {
+            IsSuccess = false;
+            ReasonPhrase += errorMessage + "\n";
+        }
+        public T? Value { get; set; }
+    }
+}
