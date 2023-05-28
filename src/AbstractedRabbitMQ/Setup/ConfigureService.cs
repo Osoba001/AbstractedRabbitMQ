6f0ea4e3cc3bf5dca117e3a78f4d200c6359a1d2
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-28 07:38:17 +0100

    Modified

--- src/AbstractedRabbitMQ/Setup/ConfigureService.cs
+++ src/AbstractedRabbitMQ/Setup/ConfigureService.cs
@@ -12,20 +12,20 @@ namespace AbstractedRabbitMQ.Setup
             return services;
         }
 
-        public static IServiceCollection AddRabbitMQPublisher(this IServiceCollection services,IServiceProvider serviceProvider, string exchangeName, string exchangeType, TimeSpan? timeToLive,
+        public static IServiceCollection AddRabbitMQPublisher(this IServiceCollection services, string exchangeName, string exchangeType, TimeSpan? timeToLive,
            bool durable = true, bool autodelete = false)
         {
-            services.AddScoped<IPublisher>(x => new Publisher(serviceProvider.GetRequiredService<IConnectionProvider>(),
+            services.AddScoped<IPublisher>(x => new Publisher(x.GetRequiredService<IConnectionProvider>(),
                 exchangeName,exchangeType,timeToLive,durable,autodelete));
             return services;
         }
         public static IServiceCollection AddRabbitMQSubscriber(this IServiceCollection services, 
-            IServiceProvider serviceProvider, string exchange, 
+             string exchange, 
             string exchangeType, string queue, string routingKey, 
             TimeSpan? timeToLive, ushort prefetchCount = 5,
             bool durable = true, bool exclusive = false, bool autodelete = false)
         {
-            services.AddScoped<ISubscriber>(x => new Subscriber(serviceProvider.GetRequiredService<IConnectionProvider>(),
+            services.AddScoped<ISubscriber>(x => new Subscriber(x.GetRequiredService<IConnectionProvider>(),
                 exchange, exchangeType,queue,routingKey, timeToLive, prefetchCount,durable,exclusive,autodelete));
             return services;
         }
