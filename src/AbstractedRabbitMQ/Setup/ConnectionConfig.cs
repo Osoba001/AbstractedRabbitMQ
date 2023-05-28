3892343bd79f40cf497fe8da0991748b758d864a
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-28 13:02:26 +0100

    a model that hold connection configuration data

---
+++ src/AbstractedRabbitMQ/Setup/ConnectionConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractedRabbitMQ.Setup
+{
+    public class ConnectionConfig
+    {
+        public string Url { get; set; }
+        public string? ClientProvideName { get; set; }
+    }
+}
