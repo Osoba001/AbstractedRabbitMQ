c025c83d87512d11083b38dfd4da787d752becfa
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-28 12:56:30 +0100

    ExchangeType as Enums

---
+++ src/AbstractedRabbitMQ/Constants/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractedRabbitMQ.Constants
+{
+    public enum ExchangeTypeRMQ
+    {
+        direct,topic ,fanout,headers
+    }
+}
