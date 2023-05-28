e2adc11c2d379462a9bf69f4267a3913d852e375
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-28 13:01:34 +0100

    a model that hold publisher configuration data

---
+++ src/AbstractedRabbitMQ/Publishers/PublisherConfig.cs
@@ -0,0 +1,21 @@
+﻿using AbstractedRabbitMQ.Constants;
+using RabbitMQ.Client;
+
+namespace AbstractedRabbitMQ.Publishers
+{
+    public class PublisherConfig
+    {
+        public PublisherConfig()
+        {
+            durable= true;
+            timeToLive=TimeSpan.FromMinutes(1);
+            exchange = "default-direct-exchange";
+            exchangeType = ExchangeTypeRMQ.direct;
+        }
+        public string exchange { get; set; }
+        public ExchangeTypeRMQ exchangeType { get; set; }
+        public TimeSpan timeToLive { get; set; }
+        public bool durable { get; set;}
+        public bool autodelete { get; set; }
+    }
+}
